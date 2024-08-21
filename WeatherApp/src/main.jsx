960b4c780a10cdfabfbae9667971ee9b000df296
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-08-21 11:59:34 +0530

    Task Done

--- WeatherApp/src/main.jsx
+++ WeatherApp/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Weather from './Weather'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Weather/>
   </StrictMode>,
 )
