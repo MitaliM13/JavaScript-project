f4a18e3333282c8f1aff4cabfddc4e1fd7842b1e
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-08-22 19:23:52 +0530

    Almost there

--- ChatApp/client/src/App.jsx
+++ ChatApp/client/src/App.jsx
@@ -1,11 +1,34 @@
 // import React from 'react'
+import { useEffect } from 'react'
 import {io} from 'socket.io-client'
+import {Container, Typography } from '@mui/material'
 
 const App = () => {
 
   const socket = io(`http://localhost:3000`)
 
-  return <div>App</div>
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(`connected`, socket.id);
+    })
+
+    socket.on("Welcome", (s) => {
+      console.log(s);
+    })
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
+  return (
+    <Container maxWidth = "sm">
+      <Typography variant='h1' component="div" gutterBottom>
+          Welcome to socket.io
+      </Typography>
+    </Container>
+  )
+
 }
 
 export default App
