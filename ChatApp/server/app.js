7d999b0c1c35b45446d03c872b4f4a4b3316b57e
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-08-23 17:20:36 +0530

    App almost done

--- ChatApp/server/app.js
+++ ChatApp/server/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { Server } from "socket.io"
-import {createServer} from "http"
+import { createServer } from "http"
 import cors from 'cors'
 
 const port = 3000
@@ -9,32 +9,41 @@ const app = express()
 const server = createServer(app)
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-        Credentials: true
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
 })
 
 app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("Byeeee!")
+  res.send("Server started")
 })
 
 io.on("connection", (socket) => {
+  console.log("User Connected", socket.id)
 
-    console.log("user Connected",socket.id);
+  // Join Room
+  socket.on("join-room", (room) => {
+    socket.join(room)
+    console.log(`${socket.id} joined room: ${room}`)
+  })
 
-    socket.on("message", (data) => {
-        console.log(data);  
-    })
+  // Send Message to Room
+  socket.on("message", ({ room, message }) => {
+    console.log(`Message from room ${room}:`, message)
+    
+    // Emit the message to the room
+    io.to(room).emit("received-message", message)
+  })
 
-    socket.on("disconnect", () => {
-        console.log(`${socket.id} disconnected`);
-    })
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`)
+  })
 })
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`)
 })
